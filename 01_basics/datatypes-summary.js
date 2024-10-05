7bb8bf5b0320dd64db781b9aa85ac9b90f58018c
junedpathan11 <junedpathan8160@gmail.com>
2024-10-05 06:48:25 +0000

    datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -20,16 +20,43 @@ console.log(id === anotherId);
 // Array, Objects, Functions
 
  
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "hitesh",
-    age: 22,
+//const heros = ["shaktiman", "naagraj", "doga"];
+//let myObj = {
+  //  name: "hitesh",
+    //age: 22,
+//}
+
+//const myFunction = function(){
+  //  console.log("Hello World");   
+//}
+
+//console.log(typeof anotherId);
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "hiteshchaudharydotcom"
+
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
 
-const myFunction = function(){
-    console.log("Hello World");   
-}
+let userTwo = userOne
 
-console.log(typeof anotherId);
+userTwo.email = "hitesh@google.com"
 
-https://262.ecma-international.org/5.1/#sec-11.4.3
+console.log(userOne.email);
+console.log(userTwo.email);
+
