423a443cd07166c5d9437dd990b51d214a839bd4
junedpathan11 <junedpathan8160@gmail.com>
2024-10-26 17:19:26 +0000

    one.js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,31 @@
+// if
+const isUserLoggedIn = true;
+const temprature = 41;
+
+if (temprature === 41)
+{
+    console.log("less than 50");
+}
+else{
+    console.log("temprature is greater than 50")
+}
+console.log("execute");
+
+
+//<, >, <=, >=, ==, !=
+
+
+// const score = 200;
+
+// if (score > 100)
+// {
+//     let power = "fly"
+//     console.log(`use power: ${power}`);
+// }
+
+// console.log(`use power: ${power}`);
+    
+
+const balance = 1000
+
+if (balance > 500) console.log("test")
