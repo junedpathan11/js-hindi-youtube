f46f208acb4e6f52774e8d9f1e1a1fbce3da7670
junedpathan11 <junedpathan8160@gmail.com>
2024-11-14 18:36:54 +0000

    one.js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -26,6 +26,36 @@ console.log("execute");
 // console.log(`use power: ${power}`);
     
 
-const balance = 1000
+// const balance = 1000
 
-if (balance > 500) console.log("test")
+// if (balance > 500)
+//     {
+//         console.log("less then 500")
+//     } else  if (balance < 750)
+//     {
+//         console.log("less then 750")
+//     } else if (balance < 900)
+//     {
+//         console.log("less then 750");
+//     } else
+//     {
+//         console.log("less than 1200");
+        
+//     }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard)
+{
+    console.log("Allow to buy courses");
+    
+}
+
+if (loggedInFromGoogle || loggedInFromEmail)
+{
+    console.log("User logged in");
+    
+}
